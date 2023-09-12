40b92a6fd16abafa39598ab6d4c670bfad90c784
Nigel Redding <nvredding2@gmail.com>
2023-09-12 13:44:24 -0400

    Sorting example

--- examples/run.py
+++ examples/run.py
@@ -1,4 +1,4 @@
 from mini.runner import run
 
 if __name__ == '__main__':
-    print(run('/home/nigel/Personal/code/proj/mini/examples/lang_examples/list.mini'))
+    print(run('/home/nigel/Personal/code/proj/mini/examples/lang_examples/sort.mini'))
